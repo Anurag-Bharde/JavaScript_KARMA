75d4026703d154e09a1362aa4f5ee27c86f2af76
Anurag_Bharde <anwolfragspitter99@gmail.com>
2024-02-09 18:28:48 +0530

    fifth

--- servPracc/index.js
+++ servPracc/index.js
@@ -1,10 +1,34 @@
 
+// const express=require('express');
+
+// const port= 3000
+// const app=express();
+
+// app.post('/chalaja',function(req,res){
+//     console.log(req.headers["authorization"])
+// })
+// app.get('/piku',function(req,res){
+//     res.json([{
+//         name:"Anurag",
+//         age:22
+//     },{
+//         name:"Pokimane",
+//         age:191
+//     }])
+// })
+
+// app.listen(port)
+
+
 const express=require('express');
 
 const port= 3000
 const app=express();
 
-app.get('/',function(req,res){
+app.post('/chalaja',function(req,res){
+    console.log(req.headers["authorization"])
+})
+app.get('/piku',function(req,res){
     res.json([{
         name:"Anurag",
         age:22
