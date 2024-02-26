be2e727aff94f122003a1278403c7984f907ff6c
Anurag-Bharde <anwolfragspitter99@gmail.com>
2024-02-26 18:06:21 +0530

    promise

--- Practice problems(Basic)/1.5promises1.js
+++ Practice problems(Basic)/1.5promises1.js
@@ -1,19 +1,17 @@
 const fs=require("fs");
 
 function might(){
-    return new Promise(function(pintu){
-        fs.readFile("a.txt","utf-8",function(err,data){
-            pintu(data);
-        });
-    }
-    )
+    return new Promise(function(pint){
+         fs.readFile("a.txt","utf-8",function(err,data){
+            pint(data);
+         })
+    })
 }
 
-function ondd(data){
+function ondo(data){
     console.log(data);
 }
 
-
 let a=might();
 console.log(a);
-a.then(ondd)
+a.then(ondo);
