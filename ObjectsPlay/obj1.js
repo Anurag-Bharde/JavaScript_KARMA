e3a7dfebf9e1dfa55180bfdc1a6df2a750b17f11
Anurag-Bharde <anwolfragspitter99@gmail.com>
2024-05-19 11:50:33 +0530

    objects play

--- ObjectsPlay/obj1.js
+++ ObjectsPlay/obj1.js
@@ -1,9 +1,19 @@
-let per={
-    name:"abc",
-    age:24,
-    xyz: function(){
-        console.log(this.name + "is afda")
-    }
-}
+// let per={
+//     name:"abc",
+//     age:24,
+//     xyz: function(){
+//         console.log(this.name + "is afda")
+//     }
+// }
 
-per.xyz();
+// per.xyz();
+
+function person(name,age,isok,ok){
+    this.name=name;
+    this.age=age;
+    this.isok=isok;
+    this.ok=ok;
+}
+let pt=new person("afs",22,"ok","isok")
+
+console.log(pt)
